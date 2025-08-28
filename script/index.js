1fa679fb1f78165a699d33e77fc75c2edd4393f7
mrazmainrafin200 <mdazmainhossin100@gmail.com>
2025-08-28 21:50:40 +0600

    add a function

---
+++ script/index.js
@@ -0,0 +1,9 @@
+// Heart icon click and top nev heart count
+let hearts = document.getElementsByClassName('heart-icon');
+let topHeartCount = document.getElementById('top-heart-count');
+for (let i = 0; i < hearts.length; i++) {
+    hearts[i].addEventListener('click', function () {
+        let currentCount = parseInt(topHeartCount.textContent);
+        topHeartCount.textContent = currentCount + 1;
+    });
+}
