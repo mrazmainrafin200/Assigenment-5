8fe27eb37808307d052ef5bb40683fb6456738ee
mrazmainrafin200 <mdazmainhossin100@gmail.com>
2025-08-29 17:02:56 +0600

    simple change

--- script/index.js
+++ script/index.js
@@ -12,6 +12,19 @@ for (let i = 0; i < hearts.length; i++) {
 let coin = 100;
 let coinCounts = document.getElementById('coin-count');
 let callButtons = document.querySelectorAll('.call-btn')
+
+let messages = {
+    "1": document.getElementById("msg1"),
+    "2": document.getElementById("msg2"),
+    "3": document.getElementById("msg3"),
+    "4": document.getElementById("msg4"),
+    "5": document.getElementById("msg5"),
+    "6": document.getElementById("msg6"),
+    "7": document.getElementById("msg7"),
+    "8": document.getElementById("msg8"),
+    "9": document.getElementById("msg9"),
+};
+
 callButtons.forEach(buttons => {
     buttons.addEventListener('click', function () {
         let service = this.getAttribute('data-service');
@@ -21,6 +34,20 @@ callButtons.forEach(buttons => {
             coin -= 20;
             coinCounts.innerText = coin;
             alert(service + "-" + number);
+
+            Object.values(messages).forEach(msg => {
+                if (msg) {
+                    msg.classList.add("hidden");
+                    msg.classList.remove("flex");
+                }
+            });
+
+            let msgId = (index + 1).toString();
+            let msg = messages[msgId];
+            if (msg) {
+                msg.classList.remove("hidden");
+                msg.classList.add("flex");
+            }
         }
         else {
             alert("Not enough coins! Please recharge.");
